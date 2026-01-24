cb9e76193185e1634ee22b77732ec1a973e246d0
bakxriyev <kamronbekbahriyev18@gmail.com>
2026-01-24 16:39:01 +0500

    dsdssd

---
+++ ecosystem.config.ts
@@ -0,0 +1,17 @@
+// ecosystem.config.js
+module.exports = {
+  apps: [
+    {
+      name: "iman_v",
+      script: "node_modules/.bin/next",
+      args: "start -p 3000",             
+      cwd: "/home/ubuntu/iman_v",        
+      env: {
+        NODE_ENV: "production"
+      },
+      watch: false,
+      autorestart: true,
+      max_restarts: 10
+    }
+  ]
+}
