a604f809bb6dfee8a4868271b39b1cc47a8c447e
bakxriyev <kamronbekbahriyev18@gmail.com>
2026-01-24 16:44:33 +0500

    dsds

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
     {
       name: "iman_v",
       script: "node_modules/.bin/next",
-      args: "start -p 3000",             
-      cwd: "/home/ubuntu/iman_v",        
+      args: "start -p 3000",
+      cwd: "/home/ubuntu/iman_v",
       env: {
         NODE_ENV: "production"
       },
